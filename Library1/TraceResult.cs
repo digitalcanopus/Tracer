290e04ffdaafeb4eb99cc0381dd3eb586a2ca43f
digitalcanopus <100073360+digitalcanopus@users.noreply.github.com>
2022-10-02 18:36:42 +0300

    result object

---
+++ Library1/TraceResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Library1
+{
+    public class TraceResult
+    {
+        public string ClassName { get; set; }
+        public string MethodName { get; set; }
+        public long Time { get; set; }
+
+        public TraceResult(string className, string methodName, long time)
+        {
+            ClassName = className;
+            MethodName = methodName;
+            Time = time;
+        }
+    }
+}
