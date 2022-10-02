dfffa12acd7ff834f9ce0b9a53d8955db470950e
digitalcanopus <rocky-cat@mail.ru>
2022-10-02 17:50:40 +0300

    unit tests

--- Test1/UnitTest1.cs
+++ Test1/UnitTest1.cs
@@ -1,3 +1,5 @@
+using lab1;
+using Library1;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Test1
@@ -6,9 +8,87 @@ namespace Test1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CompareSleep1ToSleep2()
         {
-            Assert.IsTrue(true);
+            Tracer tr1 = new Tracer(nameof(Tracer), nameof(TracedMethods.MSleep1));
+            tr1.StartTrace();
+            TracedMethods.MSleep1();
+            tr1.StopTrace();
+
+            Tracer tr2 = new Tracer(nameof(Tracer), nameof(TracedMethods.MSleep2));
+            tr2.StartTrace();
+            TracedMethods.MSleep2();
+            tr2.StopTrace();
+
+            TraceResult res1 = tr1.GetTraceResult();
+            TraceResult res2 = tr2.GetTraceResult();
+
+            Assert.IsTrue(res1.Time > res2.Time);
+        }
+
+        [TestMethod]
+        public void CompareArithm1ToArithm2()
+        {
+            Tracer tr1 = new Tracer(nameof(Tracer), nameof(TracedMethods.SumArithmProgr1));
+            tr1.StartTrace();
+            TracedMethods.SumArithmProgr1();
+            tr1.StopTrace();
+
+            Tracer tr2 = new Tracer(nameof(Tracer), nameof(TracedMethods.SumArithmProgr2));
+            tr2.StartTrace();
+            TracedMethods.SumArithmProgr2();
+            tr2.StopTrace();
+
+            TraceResult res1 = tr1.GetTraceResult();
+            TraceResult res2 = tr2.GetTraceResult();
+
+            Assert.IsTrue(res1.Time > res2.Time);
+        }
+
+        [TestMethod]
+        public void CompareSimpleToNested()
+        {
+            Tracer tr1 = new Tracer(nameof(TracedMethods), nameof(TracedMethods.TraceMethod1));
+            tr1.StartTrace();
+            TracedMethods.TraceMethod1();
+            tr1.StopTrace();
+
+            TracedMethods trace = new TracedMethods();
+            Tracer tr2 = new Tracer(nameof(TracedMethods), nameof(TracedMethods.TraceMethodInMethod));
+            tr2.StartTrace();
+            trace.TraceMethodInMethod();
+            tr2.StopTrace();
+
+            TraceResult res1 = tr1.GetTraceResult();
+            TraceResult res2 = tr2.GetTraceResult();
+
+            Assert.IsTrue(res1.Time > res2.Time);
+        }
+
+        [TestMethod]
+        public void CompareClassNames()
+        {
+            Tracer tracer = new Tracer(nameof(Program), nameof(Program.MethodMain));
+            tracer.StartTrace();
+            Program.MethodMain();
+            tracer.StopTrace();
+
+            TraceResult result = tracer.GetTraceResult();
+
+            Assert.AreEqual("Program", result.ClassName);
+        }
+
+        [TestMethod]
+        public void CompareMethodsNames()
+        {
+            Tracer tracer = new Tracer(nameof(TracedMethods), nameof(TracedMethods.TraceMethod2));
+            tracer.StartTrace();
+            TracedMethods.TraceMethod2();
+            tracer.StopTrace();
+
+            TraceResult result = tracer.GetTraceResult();
+
+            Assert.AreEqual("TraceMethod2", result.MethodName);
         }
     }
 }
