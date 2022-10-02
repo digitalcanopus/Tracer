78daeac6809fe1adcdd0e8b3200d093bd617c275
digitalcanopus <rocky-cat@mail.ru>
2022-10-02 14:48:02 +0300

    classes for serialization

---
+++ Library1/ProcessedText.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Library1
+{
+    [Serializable]
+    [XmlRoot(ElementName = "ProcessedText")]
+    public class ProcessedText
+    {
+        [XmlArrayItem("Inf about thread")]
+        public List<TInfo> tInfo { get; set; }
+        public ProcessedText() { }
+    }
+}
---
+++ Library1/Serialize.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace Library1
+{
+    public class Serialize
+    {
+        public string JsonSer(ProcessedText text)
+        {
+            return JsonSerializer.Serialize(text);
+        }
+
+        public string XmlSer(ProcessedText text)
+        {
+            var Serializer = new XmlSerializer(typeof(ProcessedText));
+            StringBuilder strB = new StringBuilder();
+            StringWriter strW = new StringWriter(strB);
+            Serializer.Serialize(strW, text); 
+            return strW.GetStringBuilder().ToString();
+        }
+    }
+}
