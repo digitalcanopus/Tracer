ad0601fce1dde01ffdf61d42cc4ec53e9d109fb5
digitalcanopus <rocky-cat@mail.ru>
2022-10-02 12:59:06 +0300

    new method

--- Library1/TInfo.cs
+++ Library1/TInfo.cs
@@ -19,7 +19,7 @@ namespace Library1
             MethodsInfList = new List<MInfo>();
         }
 
-        //add information about methods in thread
+        //add inf about method in thread
         public void MethodInfAdd(MInfo mInfo)
         {
             MethodsInfList.Add(mInfo);
